4eeb5e910b6fd2c31189d905b0a5091f40c3c02b
Curtis <curtisodell1@gmail.com>
2023-11-29 16:35:55 -0700

    changes to top row

--- CurtisWebsite/client/src/components/App.js
+++ CurtisWebsite/client/src/components/App.js
@@ -2,7 +2,27 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>First Change</h1>;
+  
+  return (
+    <div>
+      <div id="TopRowContainer">
+        <div id="TopRowLeft"></div>
+        <div id="SpacerLeft"></div>
+        <div id="TopRowMid"></div>
+        <div id="SpacerRight"></div>
+        <div id="TopRowRight"></div>
+      </div>
+     
+      <div id="LeftColumnContainer"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <h1>Hi</h1>
+    </div>
+  )
 }
 
 export default App;
